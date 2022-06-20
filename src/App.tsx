ee8dc868d6a52f965fe0e12f54b0229b360aa3f9
McPixeI <torres.88.bcn@gmail.com>
2022-06-20 19:25:29 +0200

    store & baseApi & usersApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import styled from "styled-components";
+import { useGetAllUsersPaginatedQuery } from "./services/api/usersApi";
 
 const Title = styled.h1`
   color: palevioletred;
 `;
 
 function App() {
+  const [page, setPage] = useState(1);
+  const {
+    data: users,
+    isError,
+    isLoading,
+    isSuccess,
+  } = useGetAllUsersPaginatedQuery(page);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!users?.data) return <div>No users...</div>;
+  if (isError) return <div>Oops, ther was an error</div>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <Title>hola que ase</Title>
+        <Title>Users</Title>
+        {isSuccess && users?.data.map((user) => <p>{user.first_name}</p>)}
       </header>
     </div>
   );
---
+++ src/services/api/usersApi.ts
@@ -0,0 +1,27 @@
+import { baseApi } from "./baseApi";
+
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+export interface UserListResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
+}
+
+export const usersApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getAllUsersPaginated: build.query<UserListResponse, number | void>({
+      query: (page = 1) => `users?page=${page}`,
+    }),
+  }),
+});
+
+export const { useGetAllUsersPaginatedQuery } = usersApi;
