dded66ee87575f25e6033b33969664a2913daa2a
McPixeI <torres.88.bcn@gmail.com>
2022-06-21 13:59:19 +0200

    added typed useSelector

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { baseApi } from "../services/api/baseApi";
 import { usersApi } from "../services/api/usersApi";
 
 const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
+//Store types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+//Store typed useSelector and useDispatch
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default store;
